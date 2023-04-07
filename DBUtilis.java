f21ae3dc02199339f81ca35ee206b41623cd10eb
raghavendrasreeram <raghavachowdary184@gmail.com>
2023-04-07 15:19:53 +0530

    updated

--- DBUtilis.java
+++ DBUtilis.java
@@ -2,3 +2,9 @@ hello guys git commands are very imp...
 dont miss the classes..
 now iam add this line for review the file
 now am adding dev team
+<<<<<<< HEAD
+=======
+now iam add this line for review the file 
+its very good 
+hi im updating this one now ....
+>>>>>>> 45e90a6 (updated)
